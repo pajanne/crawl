bbbb026440a9753eae4a000d622dfb247fa2a2e9
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-11-18 15:46:29 +0000

    modified charpy bash script to be callable from any path
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/charpy@12383 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- charpy.sh
+++ charpy.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 if [ $# != 3 ]; then
-  echo "./charpy test|run|export -conf /path/to/conf/file.ini"
+  echo "charpy test|run|export -conf /path/to/conf/file.ini"
   exit 1
 fi
 
-# edit this path to point to the genlib folder, as well as to the charpy folder
-GENEDB_LIB_PATH=/home/gv1/code/genlib/python:/home/gv1/code/charpy
+echo "Python version:"
+python --version
 
-export PYTHONPATH=$PYTHONPATH:$GENEDB_LIB_PATH
+export scriptpath=${0%/*} 
 
 case "$1" in
 test) 
-    python src/unit_tests.py $@
+    python $scriptpath/src/unit_tests.py $@
     ;;
 run) 
-    python src/server.py $@
+    python $scriptpath/src/server.py $@
     ;;
 export) 
-    python src/bulk_export.py $@
+    python $scriptpath/src/bulk_export.py $@
     ;;
 *)
     echo "Unknown server command."
