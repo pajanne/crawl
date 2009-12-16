3c826b1a4c298c65d337f8ad91c6a8492b830e7d
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2009-12-16 17:00:45 +0000

    fixed bug in annotation_changes sql
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/charpy@12749 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/get_all_privates_with_dates.sql
+++ sql/get_all_privates_with_dates.sql
@@ -1,13 +1,13 @@
 select 
-	gene.uniquename as geneUniquename, mrna.uniquename as mrnauniquename, transcript.uniquename as transcriptUniquename, 
-	to_date (
-	    substr(split_part(fp.value, '; ', 2), 6, 4) || '-' || 
-	    substr(split_part(fp.value, '; ', 2), 10, 2) || '-' || 
-	    substr(split_part(fp.value, '; ', 2), 12, 2) 
-	    , 'YYYYMMDD' 
+ gene.uniquename as geneUniquename, mrna.uniquename as mrnauniquename, transcript.uniquename as transcriptUniquename, 
+ to_date (
+     substr(split_part(fp.value, '; ', 2), 6, 4) || '-' || 
+     substr(split_part(fp.value, '; ', 2), 10, 2) || '-' || 
+     substr(split_part(fp.value, '; ', 2), 12, 2) 
+     , 'YYYY-MM-DD' 
     )
-	as changedate, 
-	split_part(fp.value, '; ', 3) as changedetail
+ as changedate, 
+ split_part(fp.value, '; ', 3) as changedetail
 from featureprop fp, feature transcript, feature mrna, feature gene, feature_relationship fr, feature_relationship fr2
 where fp.value like %s = true
 and fp.type_id = 26766 
@@ -19,4 +19,5 @@ and fr2.object_id = gene.feature_id
 AND gene.type_id in ('792', '423')
 AND transcript.organism_id =  %s
 AND split_part(fp.value, '; ', 2) like %s
+
 
