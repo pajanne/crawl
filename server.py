a849fb69231a14bcbe70b23744d147b88e52a569
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-02-11 15:40:09 +0000

    forgot to pass the port variable to the factory
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/charpy@13269 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def setup_connection(thread_index):
     password = connection_details["password"]
     port = connection_details["port"]
 
-    cherrypy.thread_data.connectionFactory = ConnectionFactory(host, database, user, password)
+    cherrypy.thread_data.connectionFactory = ConnectionFactory(host, database, user, password, port)
     logger.debug ("setup connection in thread " + str(thread_index) + " ... is in thread_data? " + str(hasattr(cherrypy.thread_data, "connectionFactory")) )
 
 
