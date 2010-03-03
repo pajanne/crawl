ffd6bc3f83e84a349b3fad5f4f3d6ab133b27dcd
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-03-03 17:37:56 +0000

    Simple proof of concept command-line example.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/crawl@13383 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ tst/cli_test.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+cli+test.py
+
+Created by Giles Velarde on 2010-03-03.
+Copyright (c) 2010 Wellcome Trust Sanger Institute. All rights reserved.
+"""
+
+import sys
+import os
+import optparse
+import ConfigParser
+
+try:
+    import simplejson as json
+except ImportError:
+    import json #@UnusedImport
+
+import ropy.query
+import ropy.server
+
+from crawl.api.api import API
+
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option("-c", "--conf", dest="conf", action="store", help="the path to the configuration file")
+    
+    (options, args) = parser.parse_args()
+    if options.conf == None: sys.exit("Please supply a conf parameter.")
+
+    config = ConfigParser.ConfigParser()
+    config.read(options.conf)
+
+    # cherrypy file-configs must be valid python expressions, they get eval()ed
+    host=eval(config.get('Connection', 'host'))
+    database=eval(config.get('Connection', 'database'))
+    user=eval(config.get('Connection', 'user'))
+    password=eval(config.get('Connection', 'password'))
+
+    connectionFactory = ropy.query.ConnectionFactory(host, database, user, password)
+    
+    api = API(connectionFactory)
+    result = api.annotation_changes('5671', '2009-10-03')
+    
+    print ropy.server.Formatter(result).formatJSON()
+
+
+if __name__ == '__main__':
+    main()
+
