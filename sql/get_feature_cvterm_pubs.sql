183fd6bca5932a2a12b3e83f4fe52ccf23ed9baa
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-03-09 16:56:32 +0000

    Swapped the get_feature_cvterm_pubs queries around so that the primary pub always fomrs first.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/crawl@13452 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/get_feature_cvterm_pubs.sql
+++ sql/get_feature_cvterm_pubs.sql
@@ -1,9 +1,10 @@
-SELECT pub.uniquename 
-FROM feature_cvterm_pub
-JOIN pub on feature_cvterm_pub.pub_id = pub.pub_id
-WHERE feature_cvterm_pub.feature_cvterm_id = %(feature_cvterm_id)s
-UNION
 SELECT pub.uniquename
 FROM feature_cvterm 
 JOIN pub on feature_cvterm.pub_id = pub.pub_id
 WHERE feature_cvterm.feature_cvterm_id = %(feature_cvterm_id)s
+UNION
+SELECT pub.uniquename 
+FROM feature_cvterm_pub
+JOIN pub on feature_cvterm_pub.pub_id = pub.pub_id
+WHERE feature_cvterm_pub.feature_cvterm_id = %(feature_cvterm_id)s
+
