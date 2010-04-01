ddfb0460bbb73e32b66e0d53954d63ed7e126ccb
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-04-01 10:14:30 +0000

    The query was returning taxonIDs labelled as "value". Fixed.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/crawl@13614 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/get_orthologue.sql
+++ sql/get_orthologue.sql
@@ -1,21 +1,23 @@
 SELECT 
     f.uniquename as feature, 
     orthof.uniquename as ortho,
+    orthotype.name as orthotype,
     o.genus,
     o.species,
-    op.value
+    op.value as taxonID
 FROM feature f
 JOIN feature_relationship fr ON f.feature_id = fr.subject_id 
-    AND fr.type_id = 
+    AND fr.type_id in 
         (select cvterm.cvterm_id 
             from cvterm 
             join cv 
             on cvterm.cv_id = cvterm.cv_id 
             where cv.name = 'sequence' 
-            and cvterm.name = 'orthologous_to')
+            and (cvterm.name = 'orthologous_to' or cvterm.name = 'paralogous_to'))
 JOIN feature orthof ON fr.object_id = orthof.feature_id
-JOIN organism o on orthof.organism_id = o.organism_id
-JOIN organismprop op on o.organism_id = op.organism_id 
+JOIN cvterm orthotype ON orthof.type_id = orthotype.cvterm_id
+LEFT JOIN organism o on orthof.organism_id = o.organism_id
+LEFT JOIN organismprop op on o.organism_id = op.organism_id 
     AND op.type_id = 
     (select cvterm.cvterm_id 
         from cvterm 
@@ -24,4 +26,3 @@ JOIN organismprop op on o.organism_id = op.organism_id
         where cv.name = 'genedb_misc' 
         and cvterm.name = 'taxonId')
 WHERE f.uniquename in %(features)s
-;
