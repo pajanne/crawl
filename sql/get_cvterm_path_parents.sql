352ea843828b6e14ab3e5271b42612dc5563cc5e
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-06-04 14:30:52 +0000

    Added accessions to terms/parents results.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/crawl@13940 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/get_cvterm_path_parents.sql
+++ sql/get_cvterm_path_parents.sql
@@ -3,12 +3,14 @@ SELECT
     type.name as relationship,
     cvtermpath.pathdistance as distance,
     object.name as object,
-    object_cv.name as object_cv
+    object_cv.name as object_cv,
+    dbxref.accession
 FROM cvterm subject
 JOIN cv subject_cv ON subject.cv_id = subject_cv.cv_id AND subject_cv.name = %(cv)s
 JOIN cvtermpath ON subject.cvterm_id = cvtermpath.subject_id AND cvtermpath.pathdistance > 0
 JOIN cvterm object ON cvtermpath.object_id = object.cvterm_id 
 JOIN cv object_cv ON object.cv_id = object_cv.cv_id 
 JOIN cvterm type ON cvtermpath.type_id = type.cvterm_id
+JOIN dbxref ON object.dbxref_id = dbxref.dbxref_id
 WHERE subject.name in %(terms)s
 ORDER BY cvtermpath.pathdistance
