4208f187ac3b40d7a42ee1557ef05647bc75d289
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-06-09 13:41:24 +0000

    Fixed a bug where part_ofs returned were the same as the original feature.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/crawl@13986 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/get_locations_excluding.sql
+++ sql/get_locations_excluding.sql
@@ -14,7 +14,7 @@ JOIN cvterm type ON f.type_id = type.cvterm_id
 JOIN featureloc fl ON (f.feature_id = fl.feature_id AND fl.srcfeature_id = %(regionid)s )
 
 LEFT OUTER JOIN feature_relationship fr ON f.feature_id = fr.subject_id AND fr.type_id = (select cvterm_id from cvterm where name = 'part_of')
-LEFT OUTER JOIN feature f2 ON fr.subject_id = f2.feature_id
+LEFT OUTER JOIN feature f2 ON fr.object_id = f2.feature_id
 
 WHERE 
     (
