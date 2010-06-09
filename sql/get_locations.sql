cf0c849f06030ff3dc79c971eed6212cc580725c
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-06-09 13:37:53 +0000

    Added part_of as part of the regions/locations.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/crawl@13985 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/get_locations.sql
+++ sql/get_locations.sql
@@ -5,12 +5,16 @@ SELECT
 	fl.fmax, 
 	fl.strand,
 	fl.phase,
-	f.is_obsolete
+	f.is_obsolete,
+	f2.uniquename as part_of
 	
 FROM feature f
 
 JOIN cvterm type ON f.type_id = type.cvterm_id
 JOIN featureloc fl ON (f.feature_id = fl.feature_id AND fl.srcfeature_id = %(regionid)s )
+
+LEFT OUTER JOIN feature_relationship fr ON f.feature_id = fr.subject_id AND fr.type_id = (select cvterm_id from cvterm where name = 'part_of')
+LEFT OUTER JOIN feature f2 ON fr.subject_id = f2.feature_id
 
 WHERE 
     (fl.fmin BETWEEN %(start)s AND %(end)s ) 
