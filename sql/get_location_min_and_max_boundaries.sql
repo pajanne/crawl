e46ab2e8fbab9861480b77883177968da3bf2794
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-06-09 15:55:22 +0000

    Added the boundary sql.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/crawl@13990 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ sql/get_location_min_and_max_boundaries.sql
@@ -0,0 +1,18 @@
+SELECT 
+    
+    min(fmin) as start, 
+    max(fmax) as end 
+    
+FROM feature f
+
+JOIN featureloc fl ON (f.feature_id = fl.feature_id AND fl.srcfeature_id = %(regionid)s )
+
+
+WHERE f.type_id = (select cvterm.cvterm_id from cvterm join cv on cvterm.cv_id = cv.cv_id and cv.name = 'sequence' and cvterm.name  = 'gene')
+AND (
+    (fl.fmin BETWEEN %(start)s AND %(end)s ) 
+    OR (fl.fmax BETWEEN %(start)s AND %(end)s ) 
+    OR ( fl.fmin <= %(start)s AND fl.fmax >= %(end)s ) 
+)
+
+
