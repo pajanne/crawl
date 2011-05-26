816865f16bb2628a47608e96bab91e48bf9563f1
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2011-05-26 13:17:36 +0000

    orthologues
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/python/crawl@16176 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- sql/get_orthologue_clusters.sql
+++ sql/get_orthologue_clusters.sql
@@ -10,7 +10,13 @@ a.analysis_id,
 a.name, 
 description, 
 program, programversion, algorithm, sourcename, sourceversion,sourceuri, timeexecuted,
-organismprop.value as subject_taxonID
+organismprop.value as subject_taxonID,
+ARRAY (
+    SELECT fcc.name from feature_cvterm fc 
+    JOIN cvterm fcc ON fc.cvterm_id = fcc.cvterm_id 
+    JOIN cv fccc ON fccc.cv_id = fcc.cv_id AND fccc.name = 'genedb_products' 
+    WHERE fc.feature_id = orthof.feature_id 
+) as orthoproduct
 
 FROM feature f
 
